8c4d442bfe724edba77a173f30fc657847a6d367
Adel Knode <ann.knode@atlasschool.com>
2024-10-05 19:50:13 +0000

    what

---
+++ src/component/Greeting.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Greeting() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>Welcome to the world of React!</p>
+    </div>
+  );
+}
